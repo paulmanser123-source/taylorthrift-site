49f0cc63e868c30b920fb545bc29eb57bafb0813
Paul Manser <paul.manser123@gmail.com>
2026-04-08 23:32:37 +0100

    fix stripe es module error

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -1,6 +1,6 @@
-const Stripe = require("stripe");
+import Stripe from "stripe";
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).send("Method Not Allowed");
   }
